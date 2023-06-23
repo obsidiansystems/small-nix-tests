dbfb89f7028acdea61e46ece40b1a1135e414c64
John Ericson <John.Ericson@Obsidian.Systems>
2023-06-23 10:19:27 -0400

    Add nonce to store path for easier debugging

--- ifd.nix
+++ ifd.nix
@@ -16,7 +16,7 @@ let
 
   # Create a derivation which, when built, writes some Nix code to
   # its $out path.
-  derivation-to-import = pkgs.writeText "ifd" ''
+  derivation-to-import = pkgs.writeText "ifd-${nonce}" ''
     pkgs: pkgs.stdenv.mkDerivation {
       buildInputs = [ ${toString buildInputs} ];
       name = "test-repo-smithy-ifd";
